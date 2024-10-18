62481706c354e9e976f2b3b72d91e7490c489046
김정효 <jhyokim03@naver.com>
2024-10-18 14:51:21 +0900

    Changes to be committed: 	modified:   src/ocr.py

--- src/ocr.py
+++ src/ocr.py
@@ -8,3 +8,21 @@ image = Image.open('thumbnail.jpg')
 extracted_text = pytesseract.image_to_string(image)
 
 print(extracted_text)
+
+from django.db import models
+
+class YouTubeData(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    video_id = models.CharField(max_length=100)
+
+class ThumbnailText(models.Model):
+    text = models.TextField()
+    analysis_result = models.TextField()
+    topic = models.CharField(max_length=255)
+
+class NewsData(models.Model):
+    news_title = models.CharField(max_length=255)
+    news_link = models.URLField()
+    reliability_score = models.FloatField()
+
